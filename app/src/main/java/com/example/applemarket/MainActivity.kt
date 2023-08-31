ae151e38ef53d520ca9f640abec08af9b6ce222f
minji05 <smj95133@likelion.org>
2023-08-31 16:35:35 +0900

    TEST

---
+++ app/src/main/java/com/example/applemarket/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.applemarket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //연결 확인
+
+    }
+}
