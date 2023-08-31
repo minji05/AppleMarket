644228ac7236bd550d9a0f310be5a2794f2b8fba
minji05 <smj95133@likelion.org>
2023-09-01 05:10:18 +0900

    [FEAT] : ⚙️ 스크롤 상단 이동

--- app/src/main/java/com/example/applemarket/DetailActivity.kt
+++ app/src/main/java/com/example/applemarket/DetailActivity.kt
@@ -1,11 +1,41 @@
 package com.example.applemarket
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.applemarket.databinding.ActivityDetailBinding
+import java.text.DecimalFormat
 
 class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // MainActivity에서 넘어온 데이터 가져오기
+        val selectedItem = intent.getParcelableExtra<MyItem>("selectedItem")
+
+        // 데이터를 가져와서 해당하는 뷰에 설정
+        binding.imageView.setImageResource(selectedItem?.productImg ?: R.drawable.sample1)
+        binding.nickname.text = selectedItem?.seller
+        binding.location.text = selectedItem?.address
+
+        // 가격을 천단위로 포맷팅하여 설정
+        val priceFormat = DecimalFormat("#,###")
+        val formattedPrice = priceFormat.format(selectedItem?.price ?: 0)
+        binding.price.text = formattedPrice + "원"
+
+        binding.productName.text = selectedItem?.productName
+        binding.productContent.text = selectedItem?.explain
+
+//        // '<' 버튼 클릭 시 종료
+//        binding.backBtn.setOnClickListener {
+//            finish()
+//        }
     }
+
+
 }
--- app/src/main/java/com/example/applemarket/MyItem.kt
+++ app/src/main/java/com/example/applemarket/MyItem.kt
@@ -1,12 +1,56 @@
 package com.example.applemarket
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class MyItem(
-    val productImg:Int,
+    val productImg: Int,
     val productName: String,
     val explain: String,
     val seller: String,
     val price: Int,
     val address: String,
     val like: Int,
-    val chat: Int,)
-{}
+    val chat: Int
+) : Parcelable {
+
+    // Parcelable 인터페이스를 구현하기 위한 코드
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    //writeToParcel() 메서드는 객체의 데이터를 Parcel 객체에 쓰는 메서드
+    //각 속성들을 Parcel 객체에 쓰기 위해 사용되며, 데이터를 저장하는 역할
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeInt(productImg)
+        dest.writeString(productName)
+        dest.writeString(explain)
+        dest.writeString(seller)
+        dest.writeInt(price)
+        dest.writeString(address)
+        dest.writeInt(like)
+        dest.writeInt(chat)
+    }
+
+    //CREATOR는 Parcelable 객체를 생성하고 배열을 생성하기 위한 컴패니언 객체
+    //createFromParcel() 메서드는 Parcel 객체로부터 데이터를 읽어와 MyItem 객체를 생성
+    //newArray() 메서드는 Parcelable 객체의 배열을 생성
+    companion object CREATOR : Parcelable.Creator<MyItem> {
+        override fun createFromParcel(parcel: Parcel): MyItem {
+            return MyItem(
+                parcel.readInt(),
+                parcel.readString() ?: "",
+                parcel.readString() ?: "",
+                parcel.readString() ?: "",
+                parcel.readInt(),
+                parcel.readString() ?: "",
+                parcel.readInt(),
+                parcel.readInt()
+            )
+        }
+
+        override fun newArray(size: Int): Array<MyItem?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
